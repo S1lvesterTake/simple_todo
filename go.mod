f769d0981fed3f978fca5507d733e9a87ee609ff
silvester <silvetake@gmail.com>
2020-12-19 14:34:40 +0700

    add logging

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/S1lvesterTake/simple_todo
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/sirupsen/logrus v1.7.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	. "os"
 
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	file, err := OpenFile("todo.log", O_RDWR|O_CREATE|O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Could not open file with error: " + err.Error())
+	}
+
+	log.SetOutput(file)
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetReportCaller(true)
+}
+
 func main() {
+	log.Info("Starting TODO API")
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/healtcheck", healthCheckHandler).Methods("GET")
